11c809196cc35dbf3dcb12cc776a3d0b36f8fd95
Andaru <andarudanurdara@yahoo.com>
2023-06-24 15:51:04 +0700

    Push tugas AR VR

---
+++ AR/Assets/VuforiaLicense.cs
@@ -0,0 +1,12 @@
+
+            //This class is auto-generated, please do not overwrite
+
+            public class VuforiaLicense
+            {
+const string license = "QVFjWStIVC8vLy8vQUFBQm1TZ2w2UjFkemtzcXVuaUJjRVRVMEJ1RmdPVXBETDJkVWs4dnJ0ekJ4NTEya0JXeGNtMDlrR3VheFhGV29XYUZMTlZCT0taSFJnV3NURGVCN0dHbWhLb2I0MVZjQ0xsRyszUVJhQ2J2NzUvc1dXa0wwY01pOHgvcE0yZTJFYThlSlgyT3loRDJENjZrMjBSRmphRVFEeTZuWmZsZ05laG9Ga2FrL2U1VTl0UllQZUIxNGlZbTZJamlBbEltdUJ4RWcya21JVlY3M2ZtSkZkU3ByTmpNWmJJeStTZ2dTa1JCempEeVFiZ2hWb0M3WTMvdE5xQlBvWG0xZjNlNlI5S0t3OVMwQW1FZ3RtaXVXRXFPUlRiRjdtdmtqMG1GbWtVRDZlOWpuanZ3S2JxTHlBNDlMSlNQL2QvbjkvQURHRG5rYXg4eFc1NlBVRzM0Q1JQZmRGaHdtYXVIZUlBTDh4dm9WWTEvM2VQQVg5VmI=";
+
+                static string GetLicenseKey()
+                {
+                    return license;
+                }
+            }
